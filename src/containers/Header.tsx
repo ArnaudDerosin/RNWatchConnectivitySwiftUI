578b8a70c28f691691417859f9477f93e1fa1d64
Arnaud Derosin <arnaud.derosin@gmail.com>
2022-01-28 12:53:50 +0900

    Show reachable watch image when reachable true

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -3,11 +3,14 @@ import { Image, View } from 'react-native';
 
 import { Colors } from '../constants/Colors';
 import { ConnectivityStatus } from '../components/ConnectivityStatus';
+import { useReachability } from 'react-native-watch-connectivity';
 
 export const Header = (): JSX.Element => {
+  const isReachable = useReachability();
+
   return(
     <View style={{flexDirection: 'row', height: 'auto', borderRadius: 32, marginVertical: 16, backgroundColor: Colors.shark}} >
-      <Image style={{width: 50, height: 80, marginVertical: 16, marginHorizontal: 24}} source={require('../../assets/AppleWatch.png')} />
+      <Image style={{width: 50, height: 80, marginVertical: 16, marginLeft: 32, marginRight: 16}} source={isReachable ? require('../../assets/AppleWatchReachable.png') : require('../../assets/AppleWatch.png')} />
       <ConnectivityStatus />                
     </View>
   )
