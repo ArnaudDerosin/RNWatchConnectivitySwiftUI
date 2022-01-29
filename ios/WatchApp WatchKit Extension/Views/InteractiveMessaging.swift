54554db32d246d3192f3524eced3f86369b9c43a
Arnaud Derosin <arnaud.derosin@gmail.com>
2022-01-29 22:13:04 +0900

    Add InteractiveMessage UI and logic

--- ios/WatchApp WatchKit Extension/Views/InteractiveMessaging.swift
+++ ios/WatchApp WatchKit Extension/Views/InteractiveMessaging.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct InteractiveMessaging: View {
+    @ObservedObject var phoneConnector = PhoneConnector()
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                VStack(alignment: .leading, spacing: 5) {
+                  Text("↑ Send to App")
+                    .fontWeight(.bold)
+                    Button {
+                        self.sendMessage()
+                    } label: {
+                        Text("Send")
+                    }
+                }
+              
+                VStack(alignment: .leading, spacing: 5) {
+                  Text("↓ Receive from App")
+                    .fontWeight(.bold)
+                    Text(self.phoneConnector.receivedMessage)
+                }
+            }
+        }
+    }
+  
+    private func sendMessage() {
+        let randomNumber = String(Int.random(in: 0..<100))
+        let message: [String: Any] = ["messageWatch": randomNumber]
+        self.phoneConnector.session.sendMessage(message, replyHandler: nil) { (error) in
+            print(error.localizedDescription)
+        }
     }
 }
 
