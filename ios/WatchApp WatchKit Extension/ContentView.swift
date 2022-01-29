9bf32d445e58810d7812c0ddc030672e1aec014b
Arnaud Derosin <arnaud.derosin@gmail.com>
2022-01-29 17:30:18 +0900

    Add Text to show background transfer

--- ios/WatchApp WatchKit Extension/ContentView.swift
+++ ios/WatchApp WatchKit Extension/ContentView.swift
@@ -11,26 +11,38 @@ struct ContentView: View {
     @ObservedObject var phoneConnector = PhoneConnector()
   
     var body: some View {
-      VStack(alignment: .leading, spacing: 20) {
-          VStack(alignment: .leading, spacing: 5) {
-              Text("Send to Watch")
-              Button {
-                  self.sendMessage()
-              } label: {
-                  Text("Send")
-              }
-          }
-        
-          VStack(alignment: .leading, spacing: 5) {
-              Text("Message from App")
-              Text(self.phoneConnector.receivedMessage)
-          }
-      }
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("↑ Send to Watch")
+                    Button {
+                        self.sendMessage()
+                    } label: {
+                        Text("Send")
+                    }
+                }
+
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("↓ Message from App")
+                    Text(self.phoneConnector.receivedMessage)
+                }
+              
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("↓ User info")
+                    Text(String(self.phoneConnector.userInfo))
+                }
+              
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("↓ Application context")
+                    Text(self.phoneConnector.applicationContextMessage)
+                }
+            }
+        }
     }
   
     private func sendMessage() {
         let randomNumber = String(Int.random(in: 0..<100))
-        let message: [String: Any] = ["watchMessage": randomNumber]
+        let message: [String: Any] = ["messageWatch": randomNumber]
         self.phoneConnector.session.sendMessage(message, replyHandler: nil) { (error) in
             print(error.localizedDescription)
         }
