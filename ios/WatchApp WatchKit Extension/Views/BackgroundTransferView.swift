52006626936cfd0c00a4ed9fd8e5f87f629d24dd
Arnaud Derosin <arnaud.derosin@gmail.com>
2022-01-29 21:29:16 +0900

    Init View for Interactive and background transfer

---
+++ ios/WatchApp WatchKit Extension/Views/BackgroundTransferView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundTransferView.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Arnaud Derosin on 2022/01/29.
+//
+
+import SwiftUI
+
+struct BackgroundTransferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundTransferView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundTransferView()
+    }
+}
---
+++ ios/WatchApp WatchKit Extension/Views/InteractiveMessaging.swift
@@ -0,0 +1,20 @@
+//
+//  InteractiveMessaging.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Arnaud Derosin on 2022/01/29.
+//
+
+import SwiftUI
+
+struct InteractiveMessaging: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InteractiveMessaging_Previews: PreviewProvider {
+    static var previews: some View {
+        InteractiveMessaging()
+    }
+}
